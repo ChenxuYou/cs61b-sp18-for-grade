9904ebdd3b95fbc79e137b832bf285c84f25f5aa
ChenxuYou <dxtxbmfq@gmail.com>
2024-08-20 22:08:39 +0800

    git for config

---
+++ self-created-env/src/main/java/com/example/Arithmetic.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Arithmetic {
+    /** Computes product of two ints */
+
+    public static int product(int a, int b) {
+        return a * b;
+    }
+
+    /** Computes sum of two ints (incorrectly) */
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }   
+}
